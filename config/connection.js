4df8dddbf5d5c17db56f048d82840e9fc4c661e7
Angelee Shaw <angeleefshaw@gmail.com>
2021-03-19 17:29:24 -0400

    Testing connection

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,24 @@
 var mysql = require('mysql');
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
+const connection = mysql.createConnection({
+    host: 'xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   
-//     // Your port, if not 3306
-//     port: 3306,
+    // Your port, if not 3306
+    port: 3306,
   
-//     // Your MySQL username
-//     user: 'root',
+    // Your MySQL username
+    user: 'eqbd535643ez09w6',
   
-//     // Your MySQL password
-//     password: 'somethingsimple',
+    // Your MySQL password
+    password: 'i007qjq8gj34zy1o',
   
-//     // Name of database
-//     database: 'webdev_student_db'
-//   });
+    // Name of database
+    database: 'bx5axmgvd849v2z1'
+  });
 
-const JAWSDB_URL = 'mysql://eqbd535643ez09w6:i007qjq8gj34zy1o@xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/bx5axmgvd849v2z1'
+// const JAWSDB_URL = 'mysql://eqbd535643ez09w6:i007qjq8gj34zy1o@xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/bx5axmgvd849v2z1'
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 
 
